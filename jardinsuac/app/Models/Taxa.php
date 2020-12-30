b0203820db28285cc07c5674a2998f5195b5685d
unknown <20182451@uac.pt>
2020-12-30 16:23:57 -0100

    Métodos para os filtros adicionados

--- jardinsuac/app/Models/Taxa.php
+++ jardinsuac/app/Models/Taxa.php
@@ -17,4 +17,31 @@ class Taxa extends Model
     public function getTaxa($id) {
         return DB::table('taxas')->where('id', $id)->first();
     }
+
+    public function getGenus()
+    {
+        return DB::table('taxas')->select("genus")->get();
+    }
+
+    public function getUSDA()
+    {
+        return DB::table('taxas')->select("Growth_habit_USDA_codes_and_definitions")->get();
+    }
+
+    public function getFoliar()
+    {
+        return DB::table('taxas')->select("Foliar_retention")->get();
+    }
+
+    public function getSexual()
+    {
+        return DB::table('taxas')->select("Sexual_system")->get();
+    }
+
+    public function getNativity()
+    {
+        return DB::table('taxas')->select("Nativity_status_to_Azores")->get();
+    }
+
+
 }
