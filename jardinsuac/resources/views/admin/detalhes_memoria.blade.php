3f023730bf1c4ffce9df7a6601a8c59866302654
Gonçalo Almeida <20182448@uac.pt>
2021-01-27 00:14:30 +0000

    view detalhes_memória admin criada

--- jardinsuac/resources/views/admin/detalhes_memoria.blade.php
+++ jardinsuac/resources/views/admin/detalhes_memoria.blade.php
@@ -1,13 +1,33 @@
 @extends('admin.master')
 
 @section('content')
-    <p>
-        {{$memoria->nome_visitante}}
-    </p>
-    <p>
-        {{$memoria->estatuto}}
-    </p>
-    <p>
-        {{$memoria->faixa_etaria}}
-    </p>
+    <div class="row showBox">
+        <div class="row">
+            <h4>Ficha de Exemplar</h4>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
+            <div class="col">
+                <h6>Nome</h6>
+                    <p>{{$memoria->nome_visitante}}</p>
+                <br>
+            </div>
+
+            <div class="col">
+                <h6>Faixa etária</h6>
+                    <p>{{$memoria->faixa_etaria}}</p>
+                <br>
+            </div>
+
+            <div class="col">
+                <h6>Estatuto</h6>
+                    <p>{{$memoria->estatuto}}</p>
+                <br>
+            </div>
+
+            <div class="col">
+                <h6>Classificação</h6>
+                    <p>{{$memoria->classificacao}}</p>
+                <br>
+            </div>
+    </div>
 @endsection
