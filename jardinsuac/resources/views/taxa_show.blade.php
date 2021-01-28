5b45683b2ab4d9ce6b193075eedfe76f0d392e78
unknown <20182451@uac.pt>
2021-01-27 23:11:35 -0100

    Perfil Taxas finalizado

--- jardinsuac/resources/views/taxa_show.blade.php
+++ jardinsuac/resources/views/taxa_show.blade.php
@@ -6,10 +6,17 @@
     <br>
     <div class="container-fluid">
         <div class="row showBox">
-            <div class="col-md-5" style="">
-                <img class="img-fluid"  src="{{asset('images/jardim1.jpg')}}">
+            <div class="row">
+                <div class="col-md-4 offset-md-1" style="">
+                    <h4 class="text-center">Taxa</h4>
+                    <img class="img-fluid"  src="{{asset($taxa->imagemTaxa)}}">
+                </div>
+                <div class="col-md-4 offset-md-2" style="">
+                    <h4 class="text-center">Enquadramento</h4>
+                    <img class="img-fluid"  src="{{asset($taxa->imagemEnquadramento)}}">
+                </div>
             </div>
-            <div class="col-md-7">
+            <div class="col-md-12">
                 <div class="row">
                     <h1>{{$taxa->Nome_comum}}</h1>
                 </div>
